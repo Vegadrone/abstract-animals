7e9d8b3af12c744857352ec41cc32d93136da3a5
Vegadrone <spenaantonino@gmail.com>
2022-11-22 19:06:01 +0100

    now program show total cost and avg cost of the corp

--- src/org/azienda/italy/Main.java
+++ src/org/azienda/italy/Main.java
@@ -1,5 +1,43 @@
 package org.azienda.italy;
 
-public class Main {
+import java.time.LocalDate;
 
+public class Main {
+  public static void main(String[] args) {
+    Employee e1 = new Employee ("Gino", "Pino", LocalDate.now(), 650, 15);
+    Employee e2 = new Employee ("Paolo", "Rombi", LocalDate.now(), 2400, 8);
+    Employee e3 = new Employee ("Lello", "Francosi", LocalDate.now(), 1400, 18);
+    
+    Boss b1 = new Boss ("Porta", "Ladonna", LocalDate.now(), 35000, 10000);
+    Boss b2 = new Boss ("Marta", "Morti", LocalDate.now(), 78000, 9000);
+    
+    Person [] people = {e1, e2, e3, b1, b2};
+    
+    int minYearIncome = Integer.MAX_VALUE;
+    int maxYearIncome = Integer.MIN_VALUE;
+    
+    Person maxYearIncomePerson = null;
+    Person minYearIncomePerson = null;
+    
+    int totalCost = 0;
+    int avgCost = 0;
+    
+    for ( int i = 0; i < people.length; i++) {
+    	System.out.println(people[i]);
+    	System.out.println("----------------------------------");
+    	
+    	
+    	
+    	totalCost += people[i].getYearIncome();
+    }
+    
+    avgCost = totalCost / people.length;
+    
+    
+  System.out.println("Ecco chi prende più soldi: " + maxYearIncomePerson +
+		  "\nEcco il più povero: " + minYearIncomePerson + 
+		  "\nCosti totali azienda: " + totalCost + " " + "€" + 
+		  "\nCosti medi azienda: " + avgCost + " " + "€");
+    
+  }
 }
--- src/org/azienda/italy/Person.java
+++ src/org/azienda/italy/Person.java
@@ -1,5 +1,61 @@
 package org.azienda.italy;
 
-public class Person {
+import java.time.LocalDate;
 
+public abstract class Person {
+	
+	private String name;
+	private String surname;
+	private LocalDate dateOfBirth;
+	private String corpCode;
+	
+	public Person(String name, String surname, LocalDate dateOfBirth) {
+		setName(name);
+		setSurname(surname);
+		setDateOfBirth(dateOfBirth);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public LocalDate getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(LocalDate dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public String getCorpCode() {
+		return corpCode;
+	}
+
+	public void setCorpCode(String corpCode) {
+		
+		this.corpCode = corpCode;
+	}
+	
+	public String getFullName() {
+		return getName() + " " + getSurname() + " (" + getCorpCode() + ")";
+	}
+	
+	public abstract int getYearIncome();
+	
+	@Override
+	public String toString() {
+		return getFullName() + "\nData di Nascita; " + getDateOfBirth();
+	}
 }
